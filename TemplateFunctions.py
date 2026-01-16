820243b09603f14b322a4f13240f4acbfa235a59
fazpy <87904352+FazpyDev@users.noreply.github.com>
2026-01-16 16:26:29 +0000

    Update and rename app/template_Functions.py to TemplateFunctions.py

--- TemplateFunctions.py
+++ TemplateFunctions.py
@@ -1,15 +1,15 @@
 #template_Functions.py
 
 import os
-from utils import ErrorPrint, SuccessPrint, ColorPrint
+from app.Utils import error_print, success_print, color_print
 
-class SettingFunctionsClass(): # This is a collection of all the Functions that are used in TemplateOptions.json
+class SettingFunctions(): # This is a collection of all the Functions that are used in TemplateOptions.json
 
-    def ChangePort(self, filetype, destination_folder): # This changes the port of a flask app, a small change 
+    def change_port(self, filetype, destination_folder): # This changes the port of a flask app, a small change 
         files = os.listdir(destination_folder)
         filename = "app.py" if "app.py" in files and not "run.py" in files else "run.py"
         if not filename:
-            ErrorPrint("No app.py or run.py found in the Directory, if this is a custom template please rename the file that runs the file with either app.py or run.py")
+            error_print("No app.py or run.py found in the Directory, if this is a custom template please rename the file that runs the file with either app.py or run.py")
             return 
         
         appPath = os.path.join(destination_folder, filename)
@@ -30,10 +30,10 @@ class SettingFunctionsClass(): # This is a collection of all the Functions that 
         with open(appPath, "w") as f: # Re writes the file with the modified app.run line 
             f.writelines(lines)
         if not found: # if app.run has not been found in app.py or run.py
-            ErrorPrint("app.run has not been found!")
+            error_print("app.run has not been found!")
         else: # If there have been no previous errors, this means it was a successful port change
-            SuccessPrint("Successfully changed the Port!")
+            success_print("Successfully changed the Port!")
 
 
-    def Exit(self, running_state, *args): # This exits out of the program by changing the running State to false
+    def exit(self, running_state, *args): # This exits out of the program by changing the running State to false
         running_state["running"] = False
